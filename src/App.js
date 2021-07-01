ef5d5e8d5a2e2de0dde32552919b1ae3a6e86494
mary grace <mary.delos.reyes@accenture.com>
2021-07-01 16:57:57 +0800

    passing data via props

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import ExpenseItem from './components/ExpenseItem';
 import './App.css';
 
 function App() {
-  const para = document.createElement('p');
-  para.textContent = 'This is also visible';
-  document.getElementById('root').append(para);
+  const expense = [
+    { 
+      title: 'Car Insurance',
+      amount: 294.67, 
+      data: new Date(2021, 2, 28)
+    },
+    { title: 'Car Insurance', amount: 294.67, data: new Date(2021, 2, 28)},
+    { title: 'Car Insurance', amount: 294.67, data: new Date(2021, 2, 28)},
+    { title: 'Car Insurance', amount: 294.67, data: new Date(2021, 2, 28)},
+  ];
+
   return (
     <div className="App">
       <h2>Let's get started</h2>
+      <ExpenseItem/>
+      <ExpenseItem/>
+      <ExpenseItem/>
       <ExpenseItem/>
     </div>
   );
