c44f8c060f49ee8d13ec14e7a631649c702e85ad
mary grace <mary.delos.reyes@accenture.com>
2021-07-06 10:33:59 +0800

    added data

--- src/App.js
+++ src/App.js
@@ -5,22 +5,34 @@ import './App.css';
 function App() {
   const expense = [
     { 
-      title: 'Car Insurance',
+      title: 'Tolilet Paper',
       amount: 294.67, 
       data: new Date(2021, 2, 28)
     },
+    { title: 'New TV', amount: 294.67, data: new Date(2021, 2, 28)},
     { title: 'Car Insurance', amount: 294.67, data: new Date(2021, 2, 28)},
-    { title: 'Car Insurance', amount: 294.67, data: new Date(2021, 2, 28)},
-    { title: 'Car Insurance', amount: 294.67, data: new Date(2021, 2, 28)},
+    { title: 'New Desk', amount: 294.67, data: new Date(2021, 2, 28)},
   ];
 
   return (
     <div className="App">
       <h2>Let's get started</h2>
-      <ExpenseItem/>
-      <ExpenseItem/>
-      <ExpenseItem/>
-      <ExpenseItem/>
+      <ExpenseItem 
+      title={expense[0].title} 
+      amount={expense[0].amount} 
+      date={expense[0].date}/>
+      <ExpenseItem 
+      title={expense[1].title} 
+      amount={expense[1].amount} 
+      date={expense[1].date}/>
+      <ExpenseItem 
+      title={expense[2].title} 
+      amount={expense[2].amount} 
+      date={expense[2].date}/>
+      <ExpenseItem 
+      title={expense[3].title} 
+      amount={expense[3].amount} 
+      date={expense[3].date}/>
     </div>
   );
 }
